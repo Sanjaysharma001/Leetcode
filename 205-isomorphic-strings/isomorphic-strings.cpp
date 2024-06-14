e15318fb1d32fe3921d5f742020ea8e3c93cbb1c
Sanjaysharma001 <168165408+Sanjaysharma001@users.noreply.github.com>
2024-06-14 13:20:08 +0530

    Time: 6 ms (64.10%) | Memory: 8.1 MB (87.13%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char>mp1;
-        unordered_map<char,char>mp2;
+        // unordered_map<char,char>mp1;
+        // unordered_map<char,char>mp2;
 
-        int m = s.length();
+        // int m = s.length();
 
-        for(int i = 0 ; i<m; i++){
+        // for(int i = 0 ; i<m; i++){
 
-         char ch1 = s[i];
-         char ch2 = t[i];
+        //  char ch1 = s[i];
+        //  char ch2 = t[i];
 
-         if(mp1.find(ch1)!= mp1.end() && mp1[ch1] != ch2 || mp2.find(ch2) != mp2.end() && mp2[ch2] != ch1){
-                return false;
+        //  if(mp1.find(ch1)!= mp1.end() && mp1[ch1] != ch2 || mp2.find(ch2) != mp2.end() && mp2[ch2] != ch1){
+        //         return false;
+        //     }
+
+        //     mp1[ch1]=ch2;
+        //     mp2[ch2]=ch1;
+        // }
+        // return true;
+
+       int hash[256] = {0};
+        bool isCharmapped[256] = {0};
+        for(int i = 0; i<s.size(); i++){
+            if(hash[s[i]] == 0 && isCharmapped[t[i]] == 0){
+                hash[s[i]] =  t[i];
+                isCharmapped[t[i]] = true;
             }
-
-            mp1[ch1]=ch2;
-            mp2[ch2]=ch1;
+           }
+            for(int i = 0; i<s.size(); i++){
+                if( char(hash[s[i]]) != t[i]){
+                    return false;
+            }
         }
         return true;
     }
