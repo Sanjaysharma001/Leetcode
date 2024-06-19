728c555651b357825efe749d3840017d6abc315e
Sanjaysharma001 <168165408+Sanjaysharma001@users.noreply.github.com>
2024-06-19 12:37:51 +0530

    Time: 144 ms (5.04%) | Memory: 68.9 MB (65.37%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
 
 bool possible(vector<int> &arr, int day, int m, int k) {
-    int n = arr.size(); //size of the array
+    int n = arr.size(); 
     int cnt = 0;
     int noOfB = 0;
-    // count the number of bouquets:
+    
     for (int i = 0; i < n; i++) {
         if (arr[i] <= day) {
             cnt++;
@@ -21,7 +21,7 @@ bool possible(vector<int> &arr, int day, int m, int k) {
  int minDays(vector<int>& bloomDay, int m, int k) {
         
     long long val = m * 1ll * k * 1ll;
-    int n = bloomDay.size(); //size of the array
+    int n = bloomDay.size(); 
     if (val > n) return -1; //impossible case.
     //find maximum and minimum:
     int mini = INT_MAX, maxi = INT_MIN;
